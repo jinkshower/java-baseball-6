5a237dfd19381aa8e776e208bca684a1677056eb
jinkshower <askthem@naver.com>
2023-11-22 17:01:21 +0900

    feat(controller): 게임 시작 기능 구현

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,10 @@
+package baseball.controller;
+
+import baseball.domain.GameNumbers;
+import baseball.domain.RandomNumberGenerator;
+
+public class GameController {
+
+    public void run() {
+    }
+}
